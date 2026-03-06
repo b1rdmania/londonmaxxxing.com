554d07a54fe8999348f7b996f08465b0f71aba81
b1rdmania <102524336+b1rdmania@users.noreply.github.com>
2026-03-06 20:34:16 +0000

    copy: set topbar credit to by b1rdmania x link

--- components/MapPageClient.tsx
+++ components/MapPageClient.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useMemo, useState } from "react";
-import Link from "next/link";
 
 import Map from "@/components/Map";
 import { ECOSYSTEM_META, ECOSYSTEM_ORDER } from "@/lib/ecosystemConfig";
@@ -65,7 +64,9 @@ export default function MapPageClient({ ecosystemPoints }: MapPageClientProps) {
         <div className="topbar-brand">
           <h1>london tech heatmap 🔥</h1>
           <span className="topbar-cta">
-            [built by b1rdmania] [{ecosystemPoints.length} so far] [bit buggy, wish I&apos;d never started this...] [<Link href="/guestbook">guestbook</Link>]
+            <a href="https://x.com/b1rdmania" target="_blank" rel="noreferrer">
+              (by b1rdmania)
+            </a>
           </span>
         </div>
         <nav className="topbar-nav" aria-label="Map layers">
